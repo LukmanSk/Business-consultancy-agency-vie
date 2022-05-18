08ddbc6dac7c24a5a59284c44d4813efc4623796
unknown <skd1746289@gmail.com>
2022-05-18 18:57:21 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const nav = document.querySelector(".header");
+
+// window.addEventListener("scroll", () => {
+//   nav.classList.toggle("nav__sticky", window.scrollY > 400);
+//   console.log(window.scrollY);
+// });
