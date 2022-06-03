d5f0e0b1c92c32319d08a6882ab82607d4815962
unknown <skd1746289@gmail.com>
2022-06-03 13:44:13 +0530

    update_animation

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@
 //   nav.classList.toggle("nav__sticky", window.scrollY > 400);
 //   console.log(window.scrollY);
 // });
+
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2800,
+  reset: true,
+});
+
+sr.reveal(
+  `.text-box,.img-box, .section__head, .card,.grid__item-team,.grid__item,.filter, .text-content, .news__item,  .contact, .recent-news, .credit-info`,
+  {
+    origin: "bottom",
+    interval: 100,
+  }
+);
